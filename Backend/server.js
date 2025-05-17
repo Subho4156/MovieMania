0f07f558672702eadd3cfa4fb9076aefe07d28d6
Subho4156 <subhayanp.cse.jisu22@gmail.com>
2025-05-18 00:52:46 +0530

    Some updations for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -9,9 +9,11 @@ import { ENV_VARS } from './config/envVars.js';
 import { connectDB } from './config/db.js';
 import cookieParser from 'cookie-parser';
 import { protectRoute } from './middleware/protectRoute.js';
+import path from "path";
 
 const app = express();
 const PORT= ENV_VARS.PORT;
+const __dirname= path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 
@@ -21,6 +23,14 @@ app.use("/api/v1/tv", protectRoute, tvRoutes);
 app.use("/api/v1/search", protectRoute, searchRoute);
 app.use("/api/v1/watchlist", protectRoute, watchlistRoute);
 app.use("/api/v1/actor", protectRoute, actorRoute);
+
+if(process.env.NODE_ENV=== "production"){
+    app.use(express.static(path.join(__dirname, "/Frontend/dist")));
+
+    app.get("*", (req,res)=> {
+        res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"));
+    })
+}
 
 app.listen(PORT, ()=> {
     console.log("Server started");
